37234df45de4d0a4d0a4145304d61f8560ee06ab
Mario Mesas <mmesasro@gmail.com>
2026-02-19 11:05:41 +0100

    Add plugin descriptor and theme snapshot model

---
+++ src/main/kotlin/io/marktone/model/ThemeSnapshot.kt
@@ -0,0 +1,13 @@
+package io.marktone.model
+
+data class ThemeSnapshot(
+    val editorFontFamily: String,
+    val editorFontSize: Int,
+    val foregroundHex: String,
+    val backgroundHex: String,
+    val caretRowHex: String,
+    val selectionHex: String,
+    val hyperlinkHex: String,
+    val lineCommentHex: String,
+    val defaultCodeBackgroundHex: String,
+)
