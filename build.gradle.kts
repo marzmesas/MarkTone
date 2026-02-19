fa0fd39d0faaab6fe863f9e9862c4f41c0db11bb
Mario Mesas <mmesasro@gmail.com>
2026-02-19 11:05:33 +0100

    Initial project setup with Gradle and plugin scaffold

---
+++ build.gradle.kts
@@ -0,0 +1,50 @@
+import org.jetbrains.intellij.platform.gradle.TestFrameworkType
+
+plugins {
+    kotlin("jvm") version "2.1.20"
+    id("org.jetbrains.intellij.platform") version "2.11.0"
+}
+
+group = "io.marktone"
+version = "0.1.0"
+
+repositories {
+    mavenCentral()
+    intellijPlatform {
+        defaultRepositories()
+    }
+}
+
+dependencies {
+    intellijPlatform {
+        intellijIdeaCommunity("2024.2")
+        bundledPlugin("org.intellij.plugins.markdown")
+        testFramework(TestFrameworkType.Platform)
+    }
+
+    testImplementation(kotlin("test"))
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+intellijPlatform {
+    pluginConfiguration {
+        version = project.version.toString()
+
+        ideaVersion {
+            sinceBuild = "242"
+        }
+    }
+}
+
+tasks {
+    runIde {
+        jvmArgs("-Xmx2g")
+    }
+
+    test {
+        useJUnitPlatform()
+    }
+}
